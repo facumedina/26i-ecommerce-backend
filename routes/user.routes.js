363fce3be06ff7532c5ee11b6c4a56feb773c486
facundo <facurmedina@gmail.com>
2022-06-21 13:35:34 -0300

    Pagination, limit, promises all, destructuring

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,7 +7,7 @@ api.get('/users/:name?', userController.getUsers);
 api.get('/user', userController.getUser);
 
 //Necesito enviar un dato por que necesito traer 1 solo documento (user)
-api.get('/users/:userID', userController.getUser);
+api.get('/user/:userID', userController.getUser);
 
 //Para escribir data en el backend se suele enviar con método POST a través del body.
 api.post('/users', userController.createUser);
