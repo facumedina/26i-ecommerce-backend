2531bfcee7f73de97709608d8952d2618706ee94
facundo <facurmedina@gmail.com>
2022-06-22 22:43:25 -0300

    cambios

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,13 +8,13 @@ api.get('/users/:name?', jwtVerify, userController.getUsers);
 // api.get('/user', userController.getUser);
 
 //Necesito enviar un dato por que necesito traer 1 solo documento (user)
-api.get('/user/:userID', userController.getUser);
+api.get('/user/:userID', jwtVerify, userController.getUser);
 
 //Para escribir data en el backend se suele enviar con método POST a través del body.
 api.post('/users', userController.createUser);
 
 
-api.delete('/users/:userToDeleteID', [jwtVerify, isAdmin], userController.deleteUser);
+api.delete('/users/:userToDeleteID', [jwtVerify], userController.deleteUser);
 
 api.put('/users',jwtVerify, userController.updateUser)
 
